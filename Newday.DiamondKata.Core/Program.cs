1337d2dfb3c391e84f1c33cf33256b838a59e2ff
Rob Roe <129592564+RRoeMakesStuff@users.noreply.github.com>
2023-04-23 21:48:13 +0100

    Added missing files

---
+++ Newday.DiamondKata.Core/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Newday.DiamondKata.Core;
+
+public class Program
+{
+    private readonly IHandler _handler;
+
+    public Program(IHandler handler)
+    {
+        _handler = handler;
+    }
+
+    public void Run(string[] args)
+    {
+        _handler.Run(args);
+    }
+
+    public static void Main(string[] args)
+    {
+        var serviceProvider = new ServiceCollection()
+            .ConfigureServices()
+            .BuildServiceProvider();
+
+        var program = new Program(serviceProvider.GetService<IHandler>());
+
+        program.Run(args);
+    }
+}
